40001094e943a652cf73d1452fbd287345db9856
MOHD SAZID KHAN <9293184+mohdsazidkhan@users.noreply.github.com>
2025-05-27 11:03:54 +0530

    Live API URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
-
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const LIVE_URL = 'https://subg-backend.onrender.com/api' 
+//const LOCAL_URL = 'http://localhost:5000/api' 
+const API = axios.create({ baseURL: LIVE_URL });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
