b752c83e1355c941266fbe877c1f0f7b6267bcb3
tkwebdesigner <tofique.animation@gmail.com>
2025-09-17 11:31:51 +0530

    Articel

--- src/pages/admin/AdminArticleForm.jsx
+++ src/pages/admin/AdminArticleForm.jsx
@@ -448,7 +448,8 @@ const AdminArticleForm = () => {
               <button
                 type="submit"
                 disabled={loading}
-                className="px-4 py-2 bg-yellow-500 hover:bg-yellow-600 text-white rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-yellow-500 disabled:opacity-50 disabled:cursor-not-allowed"
+                className="px-4 py-2 bg-gradient-to-r from-yellow-500 to-red-500 text-white 
+              dark:from-yellow-600 dark:to-red-700 rounded-md font-medium focus:outline-none focus:ring-2 focus:ring-yellow-500 disabled:opacity-50 disabled:cursor-not-allowed"
               >
                 {loading ? 'Saving...' : (isEdit ? 'Update Article' : 'Create Article')}
               </button>
